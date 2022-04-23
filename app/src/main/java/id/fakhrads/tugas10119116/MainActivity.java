87761f8dc34eab0efd837cbc32ee6ce62faab483
fakhrads <fakhriadisaputra15@gmail.com>
2022-04-23 09:54:09 +0700

    Fix

--- app/src/main/java/id/fakhrads/tugas10119116/MainActivity.java
+++ app/src/main/java/id/fakhrads/tugas10119116/MainActivity.java
@@ -5,7 +5,6 @@
 package id.fakhrads.tugas10119116;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
 
         Intent Profile = new Intent(MainActivity.this, ProfileActivity.class);
         startActivity(Profile);
+        finish();
+    }
+
+    public void toLogin(View view) {
+        Intent login = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(login);
         finish();
     }
 }
